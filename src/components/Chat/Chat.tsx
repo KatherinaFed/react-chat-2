0f5d5a8880c2b7057540e8101c6cebf57b13a3b1
Katherina <fedoseeva2292@gmail.com>
2023-11-23 19:54:53 +0100

    Chat -  Info bar

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import './Chat.scss'
+import './Chat.scss';
+
+import Camera from '../../assets/camera.png';
+import Add from '../../assets/addUser.png';
+import More from '../../assets/more.png';
 
 const Chat = () => {
   return (
-    <div className='chat'>
-      Chat
+    <div className="chat">
+      <div className="chat_info">
+        <span>Katja</span>
+        <div className="chat_icons">
+          <img src={Camera} alt="camera_icon" />
+          <img src={Add} alt="add_icon" />
+          <img src={More} alt="more_icon" />
+        </div>
+      </div>
     </div>
   );
 };
