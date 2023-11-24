183d5876c7230493af7826eb80f5eaff99fb00b9
Katherina <fedoseeva2292@gmail.com>
2023-11-24 14:09:29 +0100

    mixins

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './Message.scss'
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="message owner">
+      <div className="message_info">
+        <img
+          src="https://m.media-amazon.com/images/S/pv-target-images/16627900db04b76fae3b64266ca161511422059cd24062fb5d900971003a0b70.jpg"
+          alt=""
+        />
+        <span>Just now</span>
+      </div>
+      <div className="message_content">
+        <p>Hello</p>
+        <img
+          src="https://m.media-amazon.com/images/S/pv-target-images/16627900db04b76fae3b64266ca161511422059cd24062fb5d900971003a0b70.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
