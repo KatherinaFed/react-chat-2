13c6f59e82125c2d1733aacc5a8709c11dcfcf9a
Katherina <fedoseeva2292@gmail.com>
2023-12-02 21:04:52 +0100

    Search users

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,21 +1,57 @@
-import React from 'react';
+import { KeyboardEvent, useState } from 'react';
 import './Searchbar.scss';
+import {
+  DocumentData,
+  collection,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore';
+import { db } from '../../firebase';
 
 const Searchbar = () => {
+  const [username, setUsername] = useState('');
+  const [user, setUser] = useState<DocumentData | null>(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, 'users'),
+      where('displayName', '==', username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (error) {
+      setErr(true);
+    }
+  };
+
+  const handleKey = (e: KeyboardEvent<HTMLInputElement>) =>
+    e.code === 'Enter' && handleSearch();
+
   return (
     <div className="searchbar">
       <div className="searchbar_input">
-        <input type="text" placeholder='Find a user' />
-      </div>
-      <div className="user_chat">
-        <img
-          src="https://m.media-amazon.com/images/S/pv-target-images/16627900db04b76fae3b64266ca161511422059cd24062fb5d900971003a0b70.jpg"
-          alt=""
+        <input
+          type="text"
+          placeholder="Find a user"
+          onChange={(e) => setUsername(e.target.value)}
+          onKeyDown={handleKey}
         />
-        <div className="user_chat_info">
-          <span>Katja</span>
-        </div>
       </div>
+      {err && <span>User not found!</span>}
+      {user && (
+        <div className="user_chat">
+          <img src={user?.photoURL} alt="user_avatar" />
+          <div className="user_chat_info">
+            <span>{user?.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
